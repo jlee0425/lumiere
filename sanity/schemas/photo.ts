cb75de331a2940fc6db97d13334c5f9fdf20b491
Jake Lee <40153724+jlee0425@users.noreply.github.com>
2022-02-02 23:00:45 +0900

    photo as image and nothing else

--- sanity/schemas/photo.ts
+++ sanity/schemas/photo.ts
@@ -3,9 +3,6 @@ export default {
 	title: '사진',
 	type: 'document',
 	fields: [
-		{ name: 'title', title: '타이틀', type: 'string' },
-		{ name: 'slug', type: 'slug', options: { source: 'title' } },
-		{ name: 'desc', type: 'string', title: '설명' },
 		{ name: 'photo', type: 'image', title: '사진' },
 		{
 			name: 'tag',
