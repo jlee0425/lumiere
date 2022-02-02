db087f23ad7d2c0827a19f339247060b254d1ed8
Jake Lee <40153724+jlee0425@users.noreply.github.com>
2022-02-02 23:00:05 +0900

    extend default theme

--- next/tailwind.config.js
+++ next/tailwind.config.js
@@ -4,17 +4,18 @@ module.exports = {
 		'./components/**/*.{js,ts,jsx,tsx}',
 	],
 	theme: {
-		colors: {
-			'eggshell-100': '#fbf9f3',
-			'eggshell-200': '#f5f1e4',
-			'eggshell-400': '#f0ead6',
-			'eggshell-700': '#c1a855',
-		},
 		fontFamily: {
 			sans: ['Graphik', 'sans-serif'],
 			serif: ['Merriweather', 'serif'],
 		},
-		extend: {},
+		extend: {
+			colors: {
+				'eggshell-100': '#fbf9f3',
+				'eggshell-200': '#f5f1e4',
+				'eggshell-400': '#f0ead6',
+				'eggshell-700': '#c1a855',
+			},
+		},
 	},
 	plugins: [],
 };
