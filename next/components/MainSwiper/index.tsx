e0ebd9f4f9cf3bb415444703f343f6831f9d17fd
Jake Lee <40153724+jlee0425@users.noreply.github.com>
2022-06-25 20:12:34 +0900

    Add: Swiper

---
+++ next/components/MainSwiper/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Autoplay, EffectFade, Pagination } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/effect-fade';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { ImageProps } from '../../pages';
+import Image from 'next/image'
+
+type Props = {
+	images: ImageProps[];
+};
+
+const MainSwiper = ({ images }: Props) => {
+	return (
+		<Swiper
+			effect="fade"
+			pagination={{ clickable: true, dynamicBullets: true }}
+			preloadImages
+			modules={[EffectFade, Pagination, Autoplay]}
+			loop
+			autoplay
+		>
+			{images.map((image) => (
+				<SwiperSlide key={image.key}>
+					<div className="h-screen w-screen">
+						<Image
+							src={image.url}
+							layout="fill"
+							objectFit="cover"
+							alt={image.url}
+						/>
+					</div>
+				</SwiperSlide>
+			))}
+		</Swiper>
+	);
+};
+
+export default MainSwiper;
